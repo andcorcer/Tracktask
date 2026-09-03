2afe5d533a1aa28d5380d30001e1f3a5a6e2e848
Andrés Córdova <andcorcer2006@gmail.com>
2026-09-03 11:00:18 -0600

    Created a 'Google Client ID' in order to use the google APIs, saved it as an environment variable, installed the '@react-oauth/google' dependency and wrapped App component with 'GoogleOAuthProvider' with a 'clientId'

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 // Import all dependencies
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 // Import Components
 import App from "./components/App/App.jsx";
@@ -12,10 +13,17 @@ import { store } from "./store/store.js";
 // Import Styles
 import "./index.css";
 
+// Save the client id from the environment variable to a constant
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+
+// Render the app in the root element of the HTML document, wrapping it with StrictMode, Provider for Redux store, and GoogleOAuthProvider for Google OAuth functionality.
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+        <App />
+      </GoogleOAuthProvider>
     </Provider>
   </StrictMode>,
 );
