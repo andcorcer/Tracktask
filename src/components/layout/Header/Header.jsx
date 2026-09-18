88ac13ddb03540a1a070760c73f5992b28856dd6
Andrés Córdova <andcorcer2006@gmail.com>
2026-09-18 12:44:08 -0600

    Created a new Page component 'CalendarPage', created a new route 'calendar' for it and finished all 'layout' components' ('Header', 'Layout' and 'Navbar') .jsx files

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,18 +1,26 @@
 // Import all dependencies
 import React from "react";
+import { Link } from "react-router-dom";
 
 // Import Components
-
+import Navbar from "../Navbar/Navbar.jsx";
 
 // Import Styles
-import './Header.css'
+import "./Header.css";
 
-// Header Component 
+// Header Component
 const Header = () => {
-   return (
-    <>
-    </>
-   ); 
-}
+  return (
+    <header className="header">
+      <div className="title-logo">
+        <h1 className="title">
+          <Link to="/">Tracktask</Link>
+        </h1>
+      </div>
+      <Navbar />
+    </header>
+  );
+};
 
+// Export the Header component
 export default Header;
---
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -0,0 +1,18 @@
+// Import all dependencies
+import React from "react";
+
+// Import Components
+
+
+// Import Styles
+import './CalendarPage.css'
+
+// CalendarPage Component 
+const CalendarPage = () => {
+   return (
+    <>
+    </>
+   ); 
+}
+
+export default CalendarPage;
