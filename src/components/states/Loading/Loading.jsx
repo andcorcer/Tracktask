9989e35a4fbfedb3fcd6a0a8869c603b39922d18
Andrés Córdova <andcorcer2006@gmail.com>
2026-09-25 15:32:57 -0600

    Completed states components (Loading and Error) and deleted CalendarList component

---
+++ src/components/states/Loading/Loading.jsx
@@ -0,0 +1,17 @@
+// Import all dependencies
+import React from "react";
+
+// Import Styles
+import './Loading.css'
+
+// Loading Component 
+const Loading = () => {
+   return (
+    <div className="loading-container">
+      <p className="loading-message">⏳ Loading...</p>
+    </div>
+   ); 
+}
+
+// Export the Loading component
+export default Loading;
