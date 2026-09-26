61d8a090d431789380becdf1f92a815552fdb1a9
Andrés Córdova <andcorcer2006@gmail.com>
2026-09-25 18:09:35 -0600

    Changed TodoList to also render a button to render the TodoList component and made it so that it can also render 'active' and 'archived' todos based on the viewMode passed as a prop

--- src/components/lists/TodoList/TodoList.jsx
+++ src/components/lists/TodoList/TodoList.jsx
@@ -1,50 +1,94 @@
 // Import all dependencies
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import { Plus } from "lucide-react";
 
 // Import Components
 import TodoCard from "../../items/TodoCard/TodoCard";
+import TodoInput from "../../inputs/TodoInput/TodoInput";
 
 // Import Styles
-import './TodoList.css'
+import "./TodoList.css";
 
-// TodoList Component 
-const TodoList = ({ date }) => {
+// TodoList Component
+const TodoList = ({ date, viewMode = "date" }) => {
+  // We get the todos from the redux store
+  const todos = useSelector((state) => state.todos.items);
 
-   // We get the todos from the redux store 
-   const todos = useSelector((state) => state.todos.items);
+  // Local state to render the TodoInput component conditionally
+  const [showDetails, setShowDetails] = useState(false);
 
-   // We filter the todos to disply only the ones on a given date
-   const filteredTodosForDate = todos.filter((todo) => {
-      if (todo.isDaily) {
-         // For daily todos, we check if it was created before the given date and not archived yet
-         const wasCreatedBeforeDate = todo.createdAt <= date;
-         const notArchivedYet = !todo.archivedAt || todo.archivedAt > date;
-         // We return a boolean to indicate the filter weather to inlude the todo or not
-         return wasCreatedBeforeDate && notArchivedYet;
-      } else {
-         // For non daily todos, we just check if the todo corresponds for the date it was set on
-         return todo.createdAt === date;
-      }
-   });
+  const handleOpenInput = () => {
+    setShowDetails(true);
+  };
 
-   return (
+  const handleCloseInput = () => {
+    setShowDetails(false);
+  };
+
+  // We filter the todos to display only the ones given by the viewMode prop
+  const filteredTodos = todos.filter((todo) => {
+    switch (viewMode) {
+      // We filter for all "active" or non archived todos
+      case "active":
+        return !todo.archivedAt;
+
+      // We filter for all "archived" todos
+      case "archived":
+        return !!todo.archivedAt;
+
+      // We filter for "active" todos in a given date
+      case "date":
+        if (todo.isDaily) {
+          // For daily todos, we check if it was created before the given date and not archived yet
+          const wasCreatedBeforeDate = todo.createdAt <= date;
+          const notArchivedYet = !todo.archivedAt || todo.archivedAt > date;
+          // We return a boolean to indicate the filter weather to inlude the todo or not
+          return wasCreatedBeforeDate && notArchivedYet;
+        } else {
+          // For non daily todos, we just check if the todo corresponds for the date it was set on
+          return todo.createdAt === date;
+        }
+
+      default:
+        return null;
+    }
+  });
+
+  return (
     <div className="todo-list-container">
-      {/* We render todos conditionally weather filteredTodosForDate has any items or not */}
-      {filteredTodosForDate.length > 0 ? (
-         <div className="todo-list">
-            {filteredTodosForDate.map((todo) => {
-               <TodoCard key={todo.id} todo={todo} date={date} />
-            })}
-         </div>
-      ) : (
-         <div className="empty-list">
-            <p>No todos for this date.</p>
-         </div>
+      {viewMode === "date" && (
+        <button
+          className="btn add-todo-btn"
+          onClick={handleOpenInput}
+          aria-label="Add Todo Button"
+        >
+          {<Plus size={14} />}
+        </button>
       )}
+
+      {/* We render todos conditionally weather filteredTodos has any items or not */}
+      {filteredTodos.length > 0 ? (
+        <div className="todo-list">
+          {filteredTodos.map((todo) => (
+            <TodoCard key={todo.id} todo={todo} date={date} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty-list">
+          <p>
+            {viewMode === "date" && "No todos for this date."}
+            {viewMode === "active" && "No active todos."}
+            {viewMode === "date" && "No archived todos."}
+          </p>
+        </div>
+      )}
+
+      {/* Conditionally render the TodoInput component */}
+      {showDetails && <TodoInput date={date} onClose={handleCloseInput} />}
     </div>
-   ); 
-}
+  );
+};
 
 // Export the TodoList component
 export default TodoList;
