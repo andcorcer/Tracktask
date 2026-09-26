bfc10a6c835734f5f1faa2c2e8746b804c7c874b
Andrés Córdova <andcorcer2006@gmail.com>
2026-09-25 23:30:57 -0600

    Completed modal component to render it's 'children' and handle closing itself by either pressing the esc key, clicking the X or by clicking in the modal overlay

--- src/components/details/Modal/Modal.jsx
+++ src/components/details/Modal/Modal.jsx
@@ -1,20 +1,51 @@
 // Import all dependencies
-import React from "react";
-
-// Import Components
-
+import React, { useEffect } from "react";
+import { X } from "lucide-react";
 
 // Import Styles
-import './Modal.css'
+import "./Modal.css";
 
-// Modal Component 
-const Modal = () => {
-   return (
-    <>
-    </>
-   ); 
-}
+// Modal Component
+const Modal = ({ title, onClose, children }) => {
+  // Handle Escape key
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape") {
+        onClose;
+      }
+    };
 
+    document.addEventListener("keydown", handleKeyDown);
+    // Not allow to scroll on the 'main body'
+    document.body.style.overflow = "hidden";
+
+    // Cleanup function
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      // Allow to scroll on the 'main body'
+      document.body.style.overflow = "";
+    };
+  }, [onClose]);
+
+  return (
+   <div className="modal-overlay" onClick={onClose} role="button" tabIndex={0}>
+      <div className="modal-container" onClick={(e) => e.stopPropagation}>
+
+         <div className="modal-header">
+            {title && <h3 className="modal-title">{title}</h3>}
+            <button className="btn close-modal-btn" onClick={onClose} aria-label="Close modal button">
+               <X size={20} />
+            </button>
+         </div>
+
+         <div className="modal-content">
+            {/* We render whichever component is passed between the modals opening and closing tags */}
+            {children}
+         </div>
+      </div>
+   </div>
+  );
+};
+
+// Export the Modal component
 export default Modal;
-
-// Component to black out the background while viewing the details 
